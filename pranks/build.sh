982fcba9759299b13a44e4aaf7d2d02ab683e2a6
MFernflower <machacker9000@gmail.com>
2023-08-21 20:57:11 -0400

    Add files via upload

---
+++ pranks/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+for k in ./*.com
+do
+  echo -e "\e[31mRemoving old com file $k...\e[0m"
+  rm $k
+done
+
+for f in ./*.asm
+do
+  echo -e "\e[36mProcessing $f file...\e[0m"
+  fasm $f
+done
+echo -e '\007'
