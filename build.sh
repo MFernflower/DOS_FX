4e3994d8334b41d6ed6cb8bcec838f853b801905
MFernflower <machacker9000@gmail.com>
2023-08-21 20:54:46 -0400

    should be good

--- build.sh
+++ build.sh
@@ -2,13 +2,12 @@
 for k in ./*.com
 do
   echo -e "\e[31mRemoving old com file $k...\e[0m"
-  # take action on each file. $f store current file name
   rm $k
 done
 
 for f in ./*.asm
 do
   echo -e "\e[36mProcessing $f file...\e[0m"
-  # take action on each file. $f store current file name
   fasm $f
+  echo -e '\007'
 done
