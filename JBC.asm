bfae300230cbebd96beb7fc6f4745801ac3a3985
MFernflower <machacker9000@gmail.com>
2023-08-22 23:58:33 -0400

    tweaking JBC a bit more

--- JBC.asm
+++ JBC.asm
@@ -1,11 +1,12 @@
 use16
 org 100h      
-les si,[bx]   ; small way to put 0a000h into es
 mov ax,0x0013 ; mode 13
 int 0x10      ; let's go
+les si,[bx]   ; small way to put 0a000h into es 
 mov cx,0xBFBB 
 main:
 in ax,40h     ;random number (i think its counter on clock?)
+sub di,ax
 stosb         ;write color to screen
 loop main     ;loop cx times
 mov ax,0x0100 ; Check if key touched
@@ -19,5 +20,6 @@ int 0x10
 mov dx,text
 mov ah,0x09
 int 0x21
-ret
+int 0x20
+;ret
 text db "Juice B Crypts",0x24
