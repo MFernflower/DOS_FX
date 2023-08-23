4decacd0d97f4af764ee54e77256d31c9bdee45f
MFernflower <machacker9000@gmail.com>
2023-08-23 10:29:57 -0400

    Update JBC.asm

--- JBC.asm
+++ JBC.asm
@@ -4,14 +4,14 @@ mov ax,0x0013 ; mode 13
 int 0x10      ; let's go
 les si,[bx]   ; small way to put 0a000h into es 
 mov cx,0xBFBB 
-main:
+juiceb:
 in ax,40h     ;random number (i think its counter on clock?)
 sub di,ax
 stosb         ;write color to screen
-loop main     ;loop cx times
+loop juiceb   ;loop cx times
 mov ax,0x0100 ; Check if key touched
 int 16h   ; Check if key touched
-jz main  ; if not we start allover again
+jz juiceb  ; if not we start allover again
 jnz exit ; else we go to exit routine
 
 exit:
