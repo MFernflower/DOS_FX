4d5fd6f0acd7bc2e1a1bb2892d1a390877dfe477
MFernflower <machacker9000@gmail.com>
2023-08-22 23:14:11 -0400

    Add files via upload

---
+++ JBC.asm
@@ -0,0 +1,24 @@
+use16
+org 100h      
+les si,[bx]   ; small way to put 0a000h into es
+mov ax,0x0013 ; mode 13
+int 0x10      ; let's go
+mov cx,0xBFBB 
+main:
+in ax,40h     ;random number (i think its counter on clock?)
+stosb         ;write color to screen
+loop main     ;loop cx times
+mov ax,0x0100 ; Check if key touched
+int 16h   ; Check if key touched
+jz main  ; if not we start allover again
+jnz exit ; else we go to exit routine
+
+exit:
+xor ax,ax 
+int 0x10 
+mov dx,text
+mov ah,0x09
+int 0x21
+mov ah,0x20
+int 0x21
+text db "Juice B Crypts",0x24
