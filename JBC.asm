6a35d5cfd1900e657f51b5600bdcceb0186ac857
MFernflower <machacker9000@gmail.com>
2023-08-23 21:48:39 -0400

    remove some extra bytes

--- JBC.asm
+++ JBC.asm
@@ -12,13 +12,10 @@ loop juiceb   ;loop cx times
 mov ah,0x01 ; Check if key touched
 int 0x16   ; Check if key touched
 jz juiceb  ; if not we start allover again
-jnz exit ; else we go to exit routine
-
-exit:
-xor ax,ax 
-int 0x10 
-mov dx,text
-mov ah,0x09
-int 0x21
+xor ax,ax ; clear AX
+int 0x10 ; clear screen
+mov dx,text ; exit text
+mov ah,0x09 ; exit text
+int 0x21  
 ret
 text db "Juice B Crypts",0x24
