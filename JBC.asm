0b2481175ab9f0c665c5f875dcee4daa2d9379b7
MFernflower <machacker9000@gmail.com>
2024-08-30 19:40:55 -0400

    this should be fine

--- JBC.asm
+++ JBC.asm
@@ -1,6 +1,6 @@
 use16
-org 100h 
-mov ax, $0013
+org 0x100 
+mov ax,$0013
 int 0x10
 push $A000
 pop es
@@ -8,12 +8,13 @@ xor di,di
 juiceb:
 in ax,0x40     ;random number (i think it is counter on clock?)
 sub di,ax
-stosb         ;write color to screen
-;loop juiceb   ;loop cx times
+stosb         
+dec ax
+stosb
 mov ah,0x01 ; Check if key touched
 int 0x16   ; Check if key touched
 jz juiceb  ; if not we start allover again
-xor ah,ah
+xor ax,ax
 mov al,0x03
 int 0x10 ; clear screen
 mov dx,text ; exit text
