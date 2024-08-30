823006b8bf14bde9abac7b507c604a646dc40ddf
MFernflower <machacker9000@gmail.com>
2024-08-30 18:02:59 -0400

    JBC no longer kills freedos

--- JBC.asm
+++ JBC.asm
@@ -1,25 +1,23 @@
-; DO NOT RUN THIS ON FREEDOS AS IT WILL LOCK UP THE CPU IF YOU DO
 use16
-org 100h      
-mov ax,0x0013 ; mode 13
-int 0x10      ; let's go
-les si,[bx]   ; small way to put 0a000h into es 
-mov cx,0x0AA1
+org 100h 
+mov ax, $0013
+int 0x10
+push $A000
+pop es
+xor di,di
 juiceb:
 in ax,0x40     ;random number (i think it is counter on clock?)
 sub di,ax
 stosb         ;write color to screen
-loop juiceb   ;loop cx times
+;loop juiceb   ;loop cx times
 mov ah,0x01 ; Check if key touched
 int 0x16   ; Check if key touched
 jz juiceb  ; if not we start allover again
-xor ax,ax ; clear AX
+xor ah,ah
+mov al,0x03
 int 0x10 ; clear screen
 mov dx,text ; exit text
 mov ah,0x09 ; exit text
 int 0x21  
-xor ah,ah
-mov al,0x03
-int 0x10
 ret
 text db "Juice B Crypts",0x24
