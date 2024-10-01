33fef0f5bda8e8692ed016b74f9a709c9f373c39
MFernflower <machacker9000@gmail.com>
2024-09-30 23:21:48 -0400

    use proper exit routine

--- WAFFLEHOUSE.asm
+++ WAFFLEHOUSE.asm
@@ -1,20 +1,20 @@
 use16
-org 100h 
+org 0x100
 bloop:  
 xor ax,ax
 mov ah,06
 xor cx,cx
-mov dx,184fh
+mov dx,0x184f
 mov bh,30
 int 10h
 mov dx,WHOUSE 
 mov ah,0x09
-int 21h
+int 0x21
 mov ah,0x01 ; Check if key touched
-int 16h   ; Check if key touched
+int 0x16  ; Check if key touched
 jz bloop  ; if not we start allover again
-mov al,0x03
-int 10h
-mov ah,0x4c
-int 21h
+MOV AX,0x0003 
+INT 0x10 
+MOV AX,0x4C00 
+INT 0x21      
 WHOUSE db "WAFFLE HOUSE",0x21,0x24
