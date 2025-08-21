6896d8c986a920343554bd140e76d5ad4bcd29aa
MFernflower <machacker9000@gmail.com>
2025-08-21 20:06:18 +0000

    fixed JBC

---
+++ jbc.asm
@@ -0,0 +1,24 @@
+use16
+org 0x100
+mov ax, 0x0013
+int 0x10                ; Set video mode to 320x200 pixels, 256 colors
+push word 0xa000
+pop es                  ; Set ES segment to video memory
+xor di, di              ; Initialize DI to 0
+wloop:
+in ax, 0x40          ; Get random number from timer
+sub di, ax           ; Subtract random number from DI
+stosb                ; Store byte at ES:DI and increment DI
+stosb                ; Store byte at ES:DI and increment DI
+mov ah, 0x01         ; Check if a key is pressed
+int 0x16             ; Call BIOS keyboard interrupt
+jz wloop              ; If no key is pressed, loop back
+xor ax, ax
+mov al, 0x03         ; Set video mode to 80x25 text mode
+int 0x10             ; Call BIOS video interrupt
+mov dx, text         ; Load address of exit text
+mov ah, 0x09         ; Print string function
+int 0x21             ; Call DOS interrupt
+mov ax, 0x4C00       ; Exit program
+int 0x21             ; Call DOS interrupt
+text db "Juice B Crypts", 0x24
