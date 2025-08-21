302bbe194e60f84db7767df44e4083738326db9b
MFernflower <machacker9000@gmail.com>
2025-08-21 20:07:44 +0000

    Update jbc.asm

--- jbc.asm
+++ jbc.asm
@@ -9,12 +9,11 @@ wloop:
 in ax, 0x40          ; Get random number from timer
 sub di, ax           ; Subtract random number from DI
 stosb                ; Store byte at ES:DI and increment DI
-stosb                ; Store byte at ES:DI and increment DI
+stosb                ; Store byte at ES:DI and increment DI (again)
 mov ah, 0x01         ; Check if a key is pressed
 int 0x16             ; Call BIOS keyboard interrupt
 jz wloop              ; If no key is pressed, loop back
-xor ax, ax
-mov al, 0x03         ; Set video mode to 80x25 text mode
+mov ax, 0x0003         ; Set video mode to 80x25 text mode
 int 0x10             ; Call BIOS video interrupt
 mov dx, text         ; Load address of exit text
 mov ah, 0x09         ; Print string function
