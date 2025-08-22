128ce46685b301ab0817da5daa4a9a0189138ff2
MFernflower <machacker9000@gmail.com>
2025-08-21 22:07:02 -0400

    Cleanup 13HFUCKED.asm

--- 13HFUCKED.asm
+++ 13HFUCKED.asm
@@ -1,20 +1,18 @@
 ; 13hfuck.asm - interesting video effect caused by quickly calling int 21h ah 9 while in mode 13h (this effect works in DOSBOX but is untested in anything else)
 use16
-org 100h      
+org 0x100  
 mov ax,0x0013 ; mode 13
 int 0x10      ; let's go
-;----------
+
 ; MAIN LOOP
 mloop:
 mov dx,byte_string
-mov ah,0x9
-int 21h 
-mov ah,0x1
-int 16h
+mov ah,0x09
+int 0x21
+mov ah,0x01
+int 0x16
 jz mloop
-jnz cleanup
-;----------
-cleanup:
+
 MOV AX,0x0003 
 INT 0x10 
 MOV AX,0x4C00 
