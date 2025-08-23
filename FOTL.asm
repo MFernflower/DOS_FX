c19e298573c94bf2411142b2fe6d02f22186a41e
MFernflower <machacker9000@gmail.com>
2025-08-23 00:56:31 +0000

    Rewrite of FOTL.asm

--- FOTL.asm
+++ FOTL.asm
@@ -1,24 +1,23 @@
-USE16
-ORG 0x100
+use16
+org 0x100
 
-MOV AX,0x0013
-INT 0x10
-PUSH WORD 0x0A000
-POP ES
+mov ax, 0x0013          ; mode '13h'
+int 0x10                ; Set video mode to 320x200 pixels, 256 colors
+push word 0xa000
+pop es                  ; Set ES segment to video memory
 
-WRITE_LOOP:
-MOV DX,0x40
-MOV CX,0x00FC
-REP INSB
-MOV AX,0x0100
-INT 0x16
-JZ WRITE_LOOP
+wloop:
+mov dx, 0x40          ; read a byte of data from port 0x40
+insb                  ; write this byte into video memory  
+mov ah, 0x01          ; Check if a key is pressed
+int 0x16              ; Call BIOS keyboard interrupt
+jz wloop              ; If no key is pressed, loop back
 
-MOV AX,0x0003 
-INT 0x10    
-MOV DX,txt
-MOV AH,0x09
-INT 0x21
-MOV AX,0x4C00 
-INT 0x21   
-txt db "Change my pitch up! Smack my VGA up!$"
+mov ax, 0x0003       ; clear screen 
+int 0x10             ; Call BIOS video interrupt
+mov dx, text         ; Load address of exit text
+mov ah, 0x09         ; Print string function
+int 0x21             ; Call DOS interrupt
+mov ax, 0x4C00       ; Exit program
+int 0x21             ; Call DOS interrupt
+text db "Change my pitch up! Smack my VGA up!", 0x24
