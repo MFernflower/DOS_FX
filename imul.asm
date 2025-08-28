d8d3113c73994f67df19ed41dfb884922d63bfb1
MFernflower <machacker9000@gmail.com>
2025-08-28 22:40:00 +0000

    Create imul.asm

---
+++ imul.asm
@@ -0,0 +1,31 @@
+    use16
+    org 0x100
+    
+    mov ax, 0x0013
+    int 0x10      
+    
+    push word 0xa000
+    pop es           
+    
+    mov cx, (320 * 200) / 2  
+    xor di,di
+    
+    draw_loop:
+    mov ax, [12345]
+    imul ax, 22345
+    add ax, 56789
+    mov [12345], ax
+    stosw
+    loop draw_loop
+    
+    ; Wait for a key press before exiting
+    xor ax, ax
+    int 0x16
+    
+    ; Restore original video mode (Mode 03h)
+    mov ax, 0x0003
+    int 0x10 
+    
+    ; Exit to DOS
+    mov ax, 0x4c00
+    int 0x21
