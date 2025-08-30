f292b6e8a8a4762aa7189517d89d1fa1f47fbe19
MFernflower <machacker9000@gmail.com>
2025-08-29 20:55:29 -0400

    Update T2S.asm

--- T2S.asm
+++ T2S.asm
@@ -28,5 +28,5 @@ mov ah,0x09
 int 0x21
 mov ax,0x4C00 
 int 0x21      
-battles db "It's taking too long to figure it out!$"
+battles db 0x20, 0x20, 0x20, "It's taking too long to figure it out!$"
 t2step db "Alright already!$"
