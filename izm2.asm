5f615e251c6eb458698147354db4d9c934dd9acb
MFernflower <machacker9000@gmail.com>
2025-09-03 19:10:11 +0000

    Add files via upload

---
+++ izm2.asm
@@ -0,0 +1,25 @@
+use16
+org 0x100
+push word 0x0B800
+pop es                  ; Set ES segment to video memory
+xor di, di
+main_loop:
+
+add di, 0x051D
+cmp di, 0x3E80
+mov al, [es:di]
+add al, dl
+mov [es:di], al
+
+mov ah, 0x01
+int 0x16
+jz main_loop
+    
+mov ax, 0x0003       ; clear screen 
+int 0x10             ; Call BIOS video interrupt
+mov dx, text         ; Load address of exit text
+mov ah, 0x09         ; Print string function
+int 0x21             ; Call DOS interrupt
+mov ax, 0x4C00       ; Exit program
+int 0x21             ; Call DOS interrupt
+text db "Young hustler, a little gotta last$"
