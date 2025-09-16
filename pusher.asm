8ebfe6ba1ca398bbeb7272c2e6759b68f5c256e0
MFernflower <machacker9000@gmail.com>
2025-09-16 23:58:25 +0000

    rewrite of pusher

--- pusher.asm
+++ pusher.asm
@@ -2,26 +2,22 @@
     org 0x100
     push word 0x0b800
     pop es        
-    mov di,0x0000
+    xor di, di
     draw_loop:
     mov cx, 0x27ff
-    call delay_loop
-    in al,  0x40
-    and al, 0x6F
+    loop $
+    in ax, 0x41
+    neg ax
     stosb
     cmp di, 0x0CFF 
     jne draw_loop
     xor ax,ax
     int 0x16
     erase_loop:
-    mov cx, 0x270f
-    call delay_loop
+    mov cx, 0x27ff
+    loop $
     dec di
     mov byte [ES:DI], 0x00
     cmp di, 0x0000
     jne erase_loop
     int 0x20
-    
-delay_loop:
-    loop $
-    ret
