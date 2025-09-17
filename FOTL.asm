813d1d59bbf141115fb3b9f7b6c7d1c71d36e079
MFernflower <machacker9000@gmail.com>
2025-09-17 17:00:41 +0000

    Update FOTL.asm

--- FOTL.asm
+++ FOTL.asm
@@ -18,6 +18,5 @@ int 0x10             ; Call BIOS video interrupt
 mov dx, text         ; Load address of exit text
 mov ah, 0x09         ; Print string function
 int 0x21             ; Call DOS interrupt
-mov ax, 0x4C00       ; Exit program
-int 0x21             ; Call DOS interrupt
+int 0x20
 text db "Change my pitch up! Smack my VGA up!", 0x24
