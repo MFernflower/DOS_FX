d7e84d019e1860eb4f9c3d3ff11772a7978fbcb8
MFernflower <machacker9000@gmail.com>
2025-09-17 22:05:52 +0000

    Update WAFFLEHOUSE.asm

--- WAFFLEHOUSE.asm
+++ WAFFLEHOUSE.asm
@@ -6,15 +6,14 @@ mov ah,06
 xor cx,cx
 mov dx,0x184f
 mov bh,30
-int 10h
+int 0x10
 mov dx,WHOUSE 
 mov ah,0x09
 int 0x21
-mov ah,0x01 ; Check if key touched
-int 0x16  ; Check if key touched
-jz bloop  ; if not we start allover again
-MOV AX,0x0003 
-INT 0x10 
-MOV AX,0x4C00 
-INT 0x21      
+mov ah,0x01 
+int 0x16 
+jz bloop  
+mov al,0x03
+int 0x10
+int 0x20
 WHOUSE db "WAFFLE HOUSE",0x21,0x24
