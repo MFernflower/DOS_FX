f1f94f15daea8834db4619e9daaa3d8f8bff4f33
MFernflower <machacker9000@gmail.com>
2025-09-18 21:27:42 +0000

    Update tshift.asm

--- tshift.asm
+++ tshift.asm
@@ -23,5 +23,6 @@ int 0x10             ; call bios video interrupt
 mov dx, text         ; load address of exit text
 mov ah, 0x09         ; print string function
 int 0x21             ; call dos interrupt
-ret
+int 0x20
 text db "Time Shifter 1.0 - OF CHANGING MINDS", 0x24
+
