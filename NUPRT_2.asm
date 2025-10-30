441d336c2ff2adc863a97e2c44a0daeb67606e22
MFernflower <machacker9000@gmail.com>
2025-10-30 22:32:40 +0000

    Update NUPRT_2.asm

--- NUPRT_2.asm
+++ NUPRT_2.asm
@@ -3,8 +3,8 @@ ORG 0x100
 ; NEWPORT B (NUPRT_2.asm) - named after the Vibrasphere song, not the ciggie brand 
 PUSH WORD 0x0B800
 POP ES
-MOV DX,0x41
 wloop:
+MOV DX,0x41
 INSW
 MOV AX,0x0100
 INT 0x16
