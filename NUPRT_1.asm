881d78417045aaec1f95c0ce9999b6a7e0155d4c
MFernflower <machacker9000@gmail.com>
2025-11-01 13:30:28 +0000

    reduce size of newport demos

--- NUPRT_1.asm
+++ NUPRT_1.asm
@@ -3,12 +3,12 @@ ORG 0x100
 ; NEWPORT A (NUPRT_1.asm) - named after the Vibrasphere song, not the ciggie brand 
 PUSH WORD 0x0B800
 POP ES
-WRITE_LOOP:
+ABC:
 MOV DX,0x40
 INSW
 MOV DX,0x41
 INSW
-MOV AX,0x0100
-INT 0x16
-JZ WRITE_LOOP
-INT 0x20
+in al,0x60
+dec ax
+jnz ABC
+int 0x20
