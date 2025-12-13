d207a0cf6ed223882271108806072e232d6bcffb
MFernflower <machacker9000@gmail.com>
2025-12-12 20:41:15 -0500

    Update T2S.asm

--- T2S.asm
+++ T2S.asm
@@ -1,10 +1,10 @@
 ; Dedicated to a very MSDOS virus payload inspired music video - Titanium 2 step by Battles
 use16
 org 0x100 
-mov al,0x03
+mov ax,0x3
 int 0x10
 mov dx,battles
-mov ah,0x09
+mov ah,0x9
 int 0x21
 push word 0x0B800  
 pop es           
@@ -19,14 +19,14 @@ partb:
 mov al,[es:di]
 add ax,di
 mov [es:di],al
-mov ah,0x01 ; Check if key touched
+mov ah,0x1 ; Check if key touched
 int 0x16   ; Check if key touched
 jz parta  ; if not we start allover again
-xor ax,ax ; reset screen
 int 0x10 ; reset screen
 mov dx,t2step
-mov ah,0x09
+mov ah,0x9
 int 0x21
 int 0x20 
 battles db 26 dup (0x20), "It's taking too long to figure it out!$"
 t2step db 10 dup (0x20), "Alright already!$"
+
