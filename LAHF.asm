20b70ea9d5191e8bc5ac35fde992525742aefed1
MFernflower <machacker9000@gmail.com>
2025-12-12 20:43:41 -0500

    Update LAHF.asm

--- LAHF.asm
+++ LAHF.asm
@@ -14,10 +14,10 @@ PrintLoop:
     jnz PrintLoop       
     mov dh, 0xF2        
     loop PrintLoop     
-    mov ax,0x0003
+    mov ax,0x3
     int 0x10             ; Call BIOS video interrupt
     mov dx, text         ; Load address of exit text
-    mov ah, 0x09         ; Print string function
+    mov ah, 0x9         ; Print string function
     int 0x21             
     int 0x20
     text db "Hiding in the pattern in between the rows?$"
